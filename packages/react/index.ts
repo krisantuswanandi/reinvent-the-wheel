a22f363aa119a5329c0d8a46e96e6f5e1150f1c7
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2023-09-20 23:57:08 +0700

    add react

---
+++ packages/react/index.ts
@@ -0,0 +1,19 @@
+import { render, type VElement } from "vdom";
+
+export type Component = () => VElement;
+
+export function createRoot(rootContainer: Element) {
+  return {
+    render(rootComponent: Component) {
+      if (!rootContainer) {
+        return;
+      }
+
+      const rootVElement = rootComponent();
+      const rootElement = render(rootVElement);
+      rootContainer.append(rootElement);
+    },
+  };
+}
+
+export * from "vdom";
