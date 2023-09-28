b59f83ba7098f1de61797117b21d7a77ea6c381a
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2023-09-28 16:54:09 +0700

    react useState

--- packages/react-playground/index.tsx
+++ packages/react-playground/index.tsx
@@ -1,18 +1,20 @@
 /// <reference lib="dom" />
 /// <reference lib="dom.iterable" />
 
-import { createRoot, type Component } from "react";
+import { createRoot, useState, type Component } from "react";
 
 const App: Component = () => {
+  const [count, setCount] = useState(0);
+
   function onClick() {
-    alert("Hello World!!!");
+    setCount(count + 1);
   }
 
   return (
     <div style="color: red; font-family: sans-serif">
-      <div>Hello World!!!</div>
+      <div>Count: {count}</div>
       <div style="margin: 4px 0">
-        <button onClick={onClick}>Click me</button>
+        <button onClick={onClick}>Increment</button>
       </div>
       <img src="https://cataas.com/cat" width="200px" alt="Meow" />
     </div>
--- packages/react/index.ts
+++ packages/react/index.ts
@@ -2,6 +2,25 @@ import { render, type VElement } from "vdom";
 
 export type Component = () => VElement;
 
+let update: (() => void) | null = null;
+
+const states: any[] = [];
+let stateIndex = 0;
+
+export function useState<T>(initialState: T) {
+  const _stateIndex = stateIndex++;
+  states[_stateIndex] = states[_stateIndex] || initialState;
+
+  function setState(state: T) {
+    states[_stateIndex] = state;
+    if (update) {
+      update();
+    }
+  }
+
+  return [states[_stateIndex], setState];
+}
+
 export function createRoot(rootContainer: Element) {
   return {
     render(rootComponent: Component) {
@@ -9,12 +28,17 @@ export function createRoot(rootContainer: Element) {
         return;
       }
 
-      const rootVElement = rootComponent();
-      const rootElement = render(rootVElement);
+      update = () => {
+        stateIndex = 0;
+        const rootVElement = rootComponent();
+        const rootElement = render(rootVElement);
 
-      if (rootElement) {
-        rootContainer.append(rootElement);
-      }
+        if (rootElement) {
+          rootContainer.replaceChildren(rootElement);
+        }
+      };
+
+      update();
     },
   };
 }
